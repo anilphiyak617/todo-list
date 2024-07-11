8664e23a51e99be6a08c8c0b29a2b42b1f04783a
ANIL PHIYAK <78375228+anilphiyak617@users.noreply.github.com>
2024-07-11 17:56:28 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+// Sample JavaScript File
+
+// 1. Variables
+let name = "John";
+const age = 30;
+var isStudent = true;
+
+// 2. Functions
+function greet(person) {
+    return `Hello, ${person}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+// 3. Conditionals
+if (age > 18) {
+    console.log(`${name} is an adult.`);
+} else {
+    console.log(`${name} is a minor.`);
+}
+
+// 4. Loops
+for (let i = 0; i < 5; i++) {
+    console.log(`Iteration
